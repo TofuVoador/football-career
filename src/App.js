134e5c5de6de44fe19941a7cb1c13ee126717ee7
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-04-15 19:11:19 -0300

    Game Update 1.0

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Teams from './Database/teams.json';
+import Nations from './Database/nations.json'
+import Decision from './Components/decision';
 
 function RandomNumber(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max + 1 - min) + min)
 }
 
 const positions = [
   {
     title: "ST",
-    goalsBonus: 40,
+    goalsBonus: 20,
     assistsBonus: 0,
   },{
     title: "LW",
-    goalsBonus: 32,
+    goalsBonus: 16,
     assistsBonus: 4,
   },{
     title: "RW",
-    goalsBonus: 32,
+    goalsBonus: 16,
     assistsBonus: 4,
   },{
     title: "CAM",
-    goalsBonus: 24,
+    goalsBonus: 12,
     assistsBonus: 8,
   },{
     title: "CM",
-    goalsBonus: 16,
+    goalsBonus: 8,
     assistsBonus: 12,
   },{
     title: "LM",
-    goalsBonus: 8,
+    goalsBonus: 4,
     assistsBonus: 16,
   },{
     title: "RM",
-    goalsBonus: 8,
+    goalsBonus: 4,
     assistsBonus: 16,
   },{
     title: "CDM",
@@ -45,135 +47,183 @@ const positions = [
 function App() {
   const [decisions, setDecisions]  = useState([]);
 
-  const [year, setYear] = useState(RandomNumber(2019, 2021));
+  const [year, setYear] = useState(RandomNumber(2017, 2021));
 
   const [player, setPlayer] = useState({ 
-    age: RandomNumber(17, 19),
-    potential: RandomNumber(0, 4),
-    position: GetNewPosition(),
+    potential: RandomNumber(1, 4),
+    age: 17,
+    nation: Nations[RandomNumber(0, Nations.length-1)],
     team: GetNewTeam(),
+    position: GetNewPosition(),
+    wage_M: 0,
+    overall: 68,
     totalGoals: 0,
     totalAssists: 0,
-    overall: RandomNumber(62, 68),
     champions: 0,
-    ballonDOr: 0,
+    worldCup: 0,
     prodigyAward: 0,
-    worldCup: 0
+    ballonDOr: 0
   })
 
+  const [contract, setContract] = useState(RandomNumber(2, 6))
+
   function Generate (team = null) {
+    //load all player's stats
+    let newAge = player.age+1;
     let newTeam = player.team;
-
     let newPosition = player.position;
+    let newWage = player.wage_M;
+    let newOverall = player.overall;
+    let newGoals = player.totalGoals;
+    let newAssists = player.totalAssists;
+    let newChampions = player.champions;
+    let newWorldCup = player.worldCup;
+    let newProdigyAward = player.prodigyAward;
+    let newBallonDOr = player.ballonDOr;
 
-    if(team) {
+    //pre season setup
+    if(team) {    //if they change team
       newTeam = team
       newPosition = GetNewPosition()
+      newWage = Math.floor(Math.pow((player.overall - player.age) / 2.5 + (player.potential * newTeam.power), 2)) / 10
+      setContract(RandomNumber(2,5))
+    } else {    //else if contract expires
+      let cont = contract - 1;
+      if(cont <= 0) {
+        setContract(RandomNumber(2,5))
+        newWage = Math.floor(Math.pow((player.overall - player.age) / 2.5 + (player.potential * newTeam.power), 2)) / 10
+      } else {
+        setContract(cont)
+      }
     }
 
-    let newAge = player.age+1;
-
-    let growth = (player.potential+RandomNumber(24, 32)-newAge)/3;
-
-    let newOverall = Math.floor(player.overall + growth);
-
+    //calcule the player's performance
+    let growth = RandomNumber(-2, 3)/2.0-(newAge-(27+player.potential/1.6))/3.2;
+    newOverall += growth;
     if(newOverall > 100) newOverall = 100;
+    
+    //giving the performance, set how many games did they were the starter player
+    let starting = Math.floor(8*(newOverall - (67.5 + 1.5 * newTeam.power)))
+    if(starting > 100) starting = 100
+    else if(starting < 0) starting = 0
 
-    let goalsThisYear = Math.floor(newTeam.power*newOverall/10) + RandomNumber(0, newPosition.goalsBonus)
-    let assistsThisYear = Math.floor(newTeam.power*newOverall/20) + RandomNumber(0, newPosition.assistsBonus);
+    //giving the starting rate, randomize how many goals/assists did they score
+    let goalsThisYear = RandomNumber((starting/100) * newOverall / 3, (starting/100) * newOverall / 2) + RandomNumber(0, newPosition.goalsBonus);
+    let assistsThisYear = RandomNumber((starting/100) * newOverall / 5, (starting/100) * newOverall / 4) + RandomNumber(0, newPosition.assistsBonus);
 
-    let newGoals = player.totalGoals + goalsThisYear;
-    let newAssists = player.totalAssists + assistsThisYear;
+    //add to the carrer summary
+    newGoals += goalsThisYear;
+    newAssists += assistsThisYear;
 
-    let newTitles = [];
+    //post season results
+    let newTitles = [];   //collects the awards
 
-    let awardChances = (newOverall-75);
+    if(RandomNumber(0,100) < 1) {   //Puskás
+      newTitles.push("Puskás");
+    }
 
-    let newChampions = player.champions;
-    if (RandomNumber(1, 100) < newTeam.power*newTeam.power) {
+    let ballonDOreChances = (newOverall-88)*5;
+    let prodigyChances = (newOverall-80)*5;
+
+    if (RandomNumber(0, 100) < (newOverall/100)*(newTeam.power*newTeam.power-8)) {   //Champions Trophy
       newChampions += 1;
       newTitles.push("Champions");
-      awardChances += 15;
-    }
-    
-    let newWorldCup = player.worldCup;
-    if((year+2)%4 == 0 && RandomNumber(1,100) < newOverall/12) {
-        newWorldCup += 1;
-        newTitles.push("Copa do Mundo");
-        awardChances += 15;
+      ballonDOreChances += 15;
+      prodigyChances += 15;
     }
 
-    let newBallonDOr = player.ballonDOr;
-    let newProdigyAward = player.prodigyAward;
+    if((year+2)%4 == 0 && RandomNumber(0, 100) < (newOverall/100)*(player.nation.power*player.nation.power-8)) {   //World Cup Trophy
+      newWorldCup += 1;
+      newTitles.push("Copa do Mundo");
+      ballonDOreChances += 20;
+      prodigyChances += 20;
+    }
 
-    if(newAge > 24){
-      if(newOverall >= 85 && RandomNumber(1,100) <= awardChances - 10) {
+    if(newAge >= 24){
+      if(newOverall >= 88 && RandomNumber(0,100) <= ballonDOreChances) {    //Ballon D'or
         newBallonDOr += 1;
         newTitles.push("Ballon D'Or");
       }
     } else {
-      if(newOverall >= 75 && RandomNumber(1,100) <= awardChances) {
+      if(newOverall >= 80 && RandomNumber(0,100) <= prodigyChances) {   //Prodigy
         newProdigyAward += 1;
         newTitles.push("Prodígio");
       }
     }
 
-    if(RandomNumber(1,100) <= 1) {
-      newTitles.push("Puskás");
+    //trasnfer window
+    let stay_btn = document.getElementById("decision-stay")
+    if((growth < 1 && newAge < 24+newTeam.power/2.5) || (growth <= -1 && newAge < 27+newTeam.power/2.5) || (growth < newTeam.power/2.5-4)) {
+      stay_btn.style.display = "none";    //fired by the team
+    } else {
+      stay_btn.style.display = "flex";
     }
 
+    //load two options of transfer
+    transfer = GetNewTeam();    
+    transfer2 = GetNewTeam();
+
+    if(newAge >= 30) {    //retire option
+      let retire_btn = document.getElementById("retire");
+      retire_btn.style.display = "flex";
+    }
+
+    //set pleyer
     let newplayer = {
-      age: newAge,
       potential: player.potential,
+      age: newAge,
+      nation: player.nation,
       team: newTeam,
       position: newPosition,
+      wage_M: newWage,
       overall: newOverall,
       totalGoals: newGoals,
       totalAssists: newAssists,
-      worldCup: newWorldCup,
       champions: newChampions,
-      ballonDOr: newBallonDOr,
-      prodigyAward: newProdigyAward
+      worldCup: newWorldCup,
+      prodigyAward: newProdigyAward,
+      ballonDOr: newBallonDOr
     }
-
     setPlayer(newplayer);
 
+    //set Decisions
     const newDecisions = [
       ... decisions,
       {
         year: year,
         age: newAge,
         team: newTeam,
+        wage: newWage,
         position: newPosition,
+        starting: starting,
         titles: newTitles,
         goals: goalsThisYear,
         assists: assistsThisYear,
         overall: newOverall
       }
     ];
-
     setDecisions(newDecisions)
     setYear(year+1)
-    transfer = GetNewTeam();
   }
   
   function GetNewTeam() {
     let league = Teams[RandomNumber(0, Teams.length-1)];
-    let id = RandomNumber(0, league.teams.length-1)
-    return(league.teams[id])
+    let team = league.teams[RandomNumber(0, league.teams.length-1)]
+
+    return(team)
   }
   
   function GetNewPosition() {
     let pos = positions[RandomNumber(0, positions.length-1)];
     return(pos)
   }
+
+  function Retire() {
+    console.log("Retired")
+  }
   
   let transfer = GetNewTeam();
-
-  useEffect(() => {
-    console.log("Potencial: "+player.potential)
-    Generate(); 
-  }, []);
+  let transfer2 = GetNewTeam();
 
   return (
     <>
@@ -181,28 +231,21 @@ function App() {
         <h1>Football Star</h1>
       </header>
       <div className='career'>
-        {decisions.map((d) => (
-          <div className='decision' key={d.year}>
-            <h1>{d.year} (Idade: {d.age}) - {d.team.name} ({d.position.title})</h1>
-            <div className='titles'>
-              <h1>Titles:</h1>
-              <div className='titles-list'> 
-                {d.titles.map((t) => (
-                  <p key={d.year+t}>{t}</p>
-                ))}
-              </div>
-            </div>
-            <div className='stats-year'>
-              <p>Overall: {d.overall}</p>
-              <p>Goals: {d.goals}</p>
-              <p>Assists: {d.assists}</p>
-            </div>
-          </div>
+        {decisions.map((d, index) => (
+          <Decision key={index} decision={d}/>
         ))}
       </div>
-      <a onClick={() => (Generate())}>Continuar em {player.team.name}</a>
-      <a onClick={() => (Generate(transfer))}>Transferir para {transfer.name}</a>
+      <div className='choices'>
+        <a id='decision-stay' style={{display: "none"}} onClick={() => (Generate())}>Continuar em {player.team.name}</a>
+        <a id='decision-transfer' onClick={() => (Generate(transfer))}>Transferir para {transfer.name}</a>
+        <a id='decision-transfer' onClick={() => (Generate(transfer2))}>Transferir para {transfer2.name}</a>
+        <a id='retire' style={{display: "none"}}  onClick={() => (Retire())}>Aposentar-se</a>
+      </div>
       <div className='stats'>
+        <h1>Sobre</h1>
+        <p>Potencial: {player.potential}</p>
+        <p>Seleção: {player.nation.name}</p>
+        <h1>Carreira</h1>
         <p>Gols: {player.totalGoals}</p>
         <p>Assistências: {player.totalAssists}</p>
         <p>Champions: {player.champions}</p>
---
+++ src/Components/decision.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './decision.css';
+
+const Decision = (props) => {
+  let decision = props.decision
+  return ( 
+    <div className='decision' key={decision.year}>
+      <h1 className='decision-title'>{decision.year} (Idade: {decision.age}) - Overall: {Math.floor(decision.overall*10.0)/10}</h1>
+      <div className='decision-stats'>
+        <p>Time: {decision.team.name} ({decision.position.title})</p>
+        <p>Salário Anual: {decision.wage}M</p>
+        <p>Titular: {decision.starting}%</p>
+        <p>Gols: {decision.goals}</p>
+        <p>Assistências: {decision.assists}</p>
+      </div>
+      <div className='decision-titles'>
+        <p>Títulos:</p>
+        <div className='decision-titles-list'>
+          {decision.titles.map((t) => (
+            <p key={decision.year+t}>{t}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Decision;
