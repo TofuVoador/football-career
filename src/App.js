219dcf83d6c1a918b2f55d373aaee005f883c43c
Gustavo Kumagai <iagamuk.gus@gmail.com>
2024-06-24 21:14:29 -0300

    Balanceamento Temporada 2024/25

--- src/App.js
+++ src/App.js
@@ -2311,7 +2311,12 @@ function App() {
       return b.power - a.power + Math.random();
     });
 
-    allTeams = allTeams.slice(0, allTeams.length / (4 + currentPlayer.performance));
+    allTeams = allTeams.slice(
+      Math.floor(Math.abs(28 - currentPlayer.age)),
+      allTeams.length / (4 + currentPlayer.performance)
+    );
+
+    console.log(allTeams);
 
     let interestedTeams = [];
 
