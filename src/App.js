f8574c2f00b24cea18c32e7c7d55d5ea5fef6007
Gustavo Kumagai <iagamuk.gus@gmail.com>
2025-05-31 23:10:23 -0300

    Atualização Fim de Temporada 2024/25

--- src/App.js
+++ src/App.js
@@ -1976,21 +1976,6 @@ function App() {
 		setSeasons(newSeasons);
 	}
 
-	function calculateDistance(lat1, lon1, lat2, lon2) {
-		const R = 6371; // Raio da Terra em quilômetros
-		const dLat = ((lat2 - lat1) * Math.PI) / 180;
-		const dLon = ((lon2 - lon1) * Math.PI) / 180;
-		const a =
-			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-			Math.cos((lat1 * Math.PI) / 180) *
-				Math.cos((lat2 * Math.PI) / 180) *
-				Math.sin(dLon / 2) *
-				Math.sin(dLon / 2);
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		const distance = R * c; // Distância em quilômetros
-		return distance;
-	}
-
 	function GetChampionsPosition(teams, playerTeam = null) {
 		let desc = "";
 		let newTeams = DeepClone(teams);
